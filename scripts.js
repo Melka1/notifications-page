b6abd522266665d56b289fabc22e654f62810f47
Melka Tolessa <melkatole1@gmail.com>
2023-01-28 22:51:10 +0300

    finished for now

--- scripts.js
+++ scripts.js
@@ -1,2 +1,9 @@
-import jQuery from "jquery";
+// import jQuery from "jquery";
 
+function handleRead(){
+    let elements = $(".notification").removeClass("unread")
+    let circles = $(".message--container i").css("display","none")
+    console.log($(".notification"))
+    $(".count").text("0")
+    
+}
